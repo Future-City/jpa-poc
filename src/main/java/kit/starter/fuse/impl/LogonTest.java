2c52680b30ae646fe97ddb6d410f298177fd7a74
David Han <dhan@redhat.com>
2015-02-11 17:35:22 -0800

    Added intgration for local sql table that mirrors user table

--- src/main/java/kit/starter/fuse/impl/LogonTest.java
+++ src/main/java/kit/starter/fuse/impl/LogonTest.java
@@ -16,22 +16,44 @@ public class LogonTest {
         EntityManager em = factory.createEntityManager();
         // Read the existing entries and write to console
         Query q = em.createQuery("SELECT u FROM User u");
-
+        
+        em.getTransaction().begin();
+        User u1 = generateUser();
+        em.persist(u1);
+        em.getTransaction().commit();
+        
         List<User> userList = q.getResultList();
         for (User user : userList) {
-            System.out.println(user.Name);
+            System.out.println(user.getUsername());
         }
         System.out.println("Size: " + userList.size());
+        
+        em.close();
 
         // Create new user
-        em.getTransaction().begin();
+//        em.getTransaction().begin();
+//        User user = new User();
+//        user.setName("Tom Johnson");
+//        user.setLogin("tomj");
+//        user.setPassword("pass");
+//        em.persist(user);
+//        em.getTransaction().commit();
+//
+//        em.close();
+    }
+    
+    public static User generateUser(){
         User user = new User();
-        user.setName("Tom Johnson");
-        user.setLogin("tomj");
-        user.setPassword("pass");
-        em.persist(user);
-        em.getTransaction().commit();
-
-        em.close();
+        
+        user.setUsername("test_username2");
+        user.setPassword("password2");
+        user.setEmail("testemail2@me.com");
+        user.setPhoneNumber("222-222-2222");
+        user.setChallengeQ1("challenge question2");
+        user.setChallengeAns1("challenge answer 2");
+        user.setActive(true);
+        user.setVerified(false);
+        
+        return user;
     }
 }
--- src/main/java/kit/starter/fuse/impl/User.java
+++ src/main/java/kit/starter/fuse/impl/User.java
@@ -1,5 +1,8 @@
 package kit.starter.fuse.impl;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -10,44 +13,139 @@ import javax.persistence.Table;
 @Table(name = "Users")
 public class User {
 
+    private Integer id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String phoneNumber;
+
+    private String challengeQ1;
+    private String challengeAns1;
+
+    private Date createdDate;
+    private Date lastUpdated;
+    private boolean active;
+    private boolean verified;
+
+    private String walletId;
+    private String merchantId;
+    private String investorId;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    String Name = null;
-    String Login = null;
-    String Password = null;
-
-    public int get_Id() {
+    public Integer getId() {
         return id;
     }
 
-    public void set_Id(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public String getName() {
-        return Name;
+    public String getUsername() {
+        return username;
     }
 
-    public void setName(String Name) {
-        this.Name = Name;
-    }
-
-    public String getLogin() {
-        return Login;
-    }
-
-    public void setLogin(String Login) {
-        this.Login = Login;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
-        return Password;
+        return password;
     }
 
-    public void setPassword(String Password) {
-        this.Password = Password;
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWalletId() {
+        return walletId;
+    }
+
+    public void setWalletId(String walletId) {
+        this.walletId = walletId;
+    }
+
+    public String getMerchantId() {
+        return merchantId;
+    }
+
+    public void setMerchantId(String merchantId) {
+        this.merchantId = merchantId;
+    }
+
+    public String getInvestorId() {
+        return investorId;
+    }
+
+    public void setInvestorId(String investorId) {
+        this.investorId = investorId;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+    
+    @Column(name="isactive")
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getChallengeQ1() {
+        return challengeQ1;
+    }
+
+    public void setChallengeQ1(String challengeQ1) {
+        this.challengeQ1 = challengeQ1;
+    }
+
+    public String getChallengeAns1() {
+        return challengeAns1;
+    }
+
+    public void setChallengeAns1(String challengeAns1) {
+        this.challengeAns1 = challengeAns1;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Column(name="isverified")
+    public boolean isVerified() {
+        return verified;
+    }
+
+    public void setVerified(boolean verified) {
+        this.verified = verified;
     }
 
 }
